fcb02ec66369580bb1dbfee529e1ea42de2ac148
Andrew Lahman <101824881+lahman1@users.noreply.github.com>
2024-04-27 09:55:19 -0700

    v1

---
+++ week3diss1/calculateAverage.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+double calculateAverage(int arr[], int size) {
+    double sum = 0.0;
+    
+    // Caluculate the sum of all the elments in the array
+    for (int i = 0; i < size; ++i) {
+        sum += arr[i];
+    }
+
+    // Calculate the avergae
+    double average = sum / size;
+    
+    std::cout << "The average is: " << average << std::endl;
+
+    return average;
+    }
+
+    int main() {
+        int numbers[] = {1, 10, 13, 17, 21, 30, 50}
+        int size = sizeof(numbers) / sizeof(numbers[0])
+
+        double avg = calculateAverage(numbers, size);
+
+        return 0;
+    }
